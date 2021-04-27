3dc3621b93c27757f2ca7bc6408269dcc15edabe
Gabe Rodriguez <gabe@spotify.com>
2021-04-27 16:57:03 +0200

    Adjusting takeout/dine in time

--- components/about-us/location-info/index.tsx
+++ components/about-us/location-info/index.tsx
@@ -90,7 +90,7 @@ const LocationInfo = () => (
           GOOGLE MAPS LINK
         </a>
       </p>
-      <p>We’re open Mon-Wed (3pm-9pm), Thurs-Sat (11am-9pm), and closed on Sundays.</p>
+      <p>We’re open Mon-Wed (3pm-9pm Takeout only), Thurs-Sat (11am-9pm Dine in & Takeout), and closed on Sundays.</p>
     </RedBox>
   </Container>
 );
