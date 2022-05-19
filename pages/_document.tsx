883da54379c72fc690a847ff3834215eed8fc0f7
Gabe Rodriguez <grod220@gmail.com>
2022-05-19 15:46:31 +0200

    Bring back _document.tsx to render styled components correctly

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+/*
+  Needed to render styled components correctly on the Nextjs server.
+  Should consider later how this relates to: styledComponents: true in next.config.js.
+  At the moment, this file is still necessary to prevent SSR css flicker.
+*/
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
