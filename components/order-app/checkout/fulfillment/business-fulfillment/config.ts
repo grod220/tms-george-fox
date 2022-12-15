5f3fd89aa7a2f5d9a34f6543d45343082afc1669
Gabe Rodriguez <grod220@gmail.com>
2022-12-15 20:05:16 +0100

    new locations

--- components/order-app/checkout/fulfillment/business-fulfillment/config.ts
+++ components/order-app/checkout/fulfillment/business-fulfillment/config.ts
@@ -19,6 +19,31 @@ const businessOrderConfig: BusinessOrderOption[] = [
     addr: '100 East Pine Street',
     deliveryTimes: [set(new Date(), { year: 2022, month: 11, date: 29, hours: 15, minutes: 0 })],
   },
+  {
+    buildingName: 'Maitland Paragon',
+    addr: '1060 Maitland Center Commons Blvd',
+    deliveryTimes: [set(new Date(), { year: 2023, month: 0, date: 11, hours: 16, minutes: 0 })],
+  },
+  {
+    buildingName: 'Maitland American',
+    addr: '1051 Winderely Place',
+    deliveryTimes: [set(new Date(), { year: 2023, month: 0, date: 11, hours: 16, minutes: 0 })],
+  },
+  {
+    buildingName: 'Maitland Alliance',
+    addr: '2200 Lucien Way',
+    deliveryTimes: [set(new Date(), { year: 2023, month: 0, date: 18, hours: 16, minutes: 0 })],
+  },
+  {
+    buildingName: 'Maitland Lake Lucien',
+    addr: '2201 Lucien Way',
+    deliveryTimes: [set(new Date(), { year: 2023, month: 0, date: 18, hours: 16, minutes: 0 })],
+  },
+  {
+    buildingName: 'Maitland Lucien Green',
+    addr: '2250 Lucien Way',
+    deliveryTimes: [set(new Date(), { year: 2023, month: 0, date: 18, hours: 16, minutes: 0 })],
+  },
 ];
 
 export const getBusinessOrderConfig = () =>
