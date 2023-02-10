e8b68547c7c38a1f78e6231c1bc30ca7f1010a1f
Gabe Rodriguez <grod220@gmail.com>
2023-02-10 17:49:56 +0100

    update config

--- components/order-app/checkout/fulfillment/business-fulfillment/config.ts
+++ components/order-app/checkout/fulfillment/business-fulfillment/config.ts
@@ -10,9 +10,9 @@ export interface BusinessOrderOption {
 
 const businessOrderConfig: BusinessOrderOption[] = [
   {
-    buildingName: 'CERTUS Memory Care',
-    addr: '11120 Lake Underhill Rd',
-    deliveryTimes: [set(new Date(), { year: 2023, month: 1, date: 9, hours: 15, minutes: 0 })],
+    buildingName: 'Fairwinds',
+    addr: '1st floor conference room, 135 W. Central Blvd.',
+    deliveryTimes: [set(new Date(), { year: 2023, month: 1, date: 23, hours: 16, minutes: 0 })],
   },
 ];
 
